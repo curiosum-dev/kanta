7b779e55901cfb0a95e5027f194298de39cebbaf
Krzysztof Janiec <krzysztof.janiec@Krzysztofs-MacBook-Pro.local>
2023-03-08 15:50:25 +0100

    chore: update package version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kanta.MixProject do
   def project do
     [
       app: :kanta,
-      version: "0.1.0",
+      version: "0.0.1-rc1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
