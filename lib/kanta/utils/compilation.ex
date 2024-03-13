b0fe4a35929104a2c4d8dc6154f6fb1c700c39b5
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2024-03-13 12:28:14 +0100

    Add doc to `compiling?` function

--- lib/kanta/utils/compilation.ex
+++ lib/kanta/utils/compilation.ex
@@ -1,6 +1,12 @@
 defmodule Kanta.Utils.Compilation do
   @moduledoc false
 
+  @doc """
+  Returns `true` if it is run during compilation.
+
+  This function is used to handle translation messages during compilation time in macros and module attributes.
+  """
+  @spec compiling?() :: boolean()
   def compiling? do
     Code.ensure_loaded?(Code) &&
       Code.can_await_module_compilation?()
