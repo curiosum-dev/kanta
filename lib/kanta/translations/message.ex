be37f7ca806fef11839bfb7f7f80a38cd133070e
Keisuke Kubota <Kk.bb1130@outlook.jp>
2024-06-30 12:52:07 +0200

    Add constraint to Message table

--- lib/kanta/translations/message.ex
+++ lib/kanta/translations/message.ex
@@ -32,5 +32,6 @@ defmodule Kanta.Translations.Message do
     struct
     |> cast(params, @required_fields ++ @optional_fields)
     |> validate_required(@required_fields)
+    |> unique_constraint([:msgid, :domain_id, :message_type])
   end
 end
