271787830f8b83a3aa1837f5ef7dd310e357084d
Artur Ziętkiewicz <artur.zietkiewicz@curiosum.com>
2025-02-12 14:00:38 +0100

    Create function for returning DB adapter name

--- lib/kanta/repo.ex
+++ lib/kanta/repo.ex
@@ -2,4 +2,11 @@ defmodule Kanta.Repo do
   def get_repo do
     Kanta.config().repo
   end
+
+  def get_adapter_name do
+    case get_repo().__adapter__() do
+      Ecto.Adapters.Postgres -> :postgres
+      Ecto.Adapters.SQLite3 -> :sqlite
+    end
+  end
 end
