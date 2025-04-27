4282769e4046e3a5c363d8bee31f7c40532200fd
jk-lamb <jakub.lambrych@curiosum.com>
2025-04-27 11:30:08 -0300

    Update Kanta.Repo to include Ecto.Repo functions

--- lib/kanta/repo.ex
+++ lib/kanta/repo.ex
@@ -1,4 +1,28 @@
 defmodule Kanta.Repo do
+  def aggregate(queryable, aggregate, opts \\ []) do
+    get_repo().aggregate(queryable, aggregate, opts)
+  end
+
+  def delete_all(queryable, opts \\ []) do
+    get_repo().delete_all(queryable, opts)
+  end
+
+  def get(querable, id, opts \\ []) do
+    get_repo().get(querable, id, opts)
+  end
+
+  def insert(struct_or_changeset, opts \\ []) do
+    get_repo().insert(struct_or_changeset, opts)
+  end
+
+  def one(querable, opts \\ []) do
+    get_repo().one(querable, opts)
+  end
+
+  def update(changeset, opts \\ []) do
+    get_repo().update(changeset, opts)
+  end
+
   def get_repo do
     Kanta.config().repo
   end
