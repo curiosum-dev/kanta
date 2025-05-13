c0bc9b53ca5ec9e48d35725b3f865868725fdbf3
jk-lamb <jakub.lambrych@curiosum.com>
2025-05-13 12:40:52 -0300

    Refactor translations view to use new data access patterns

--- lib/kanta_web/live/translations/translations_live/translations_live.ex
+++ lib/kanta_web/live/translations/translations_live/translations_live.ex
@@ -1,178 +1,123 @@
 defmodule KantaWeb.Translations.TranslationsLive do
+  alias Phoenix.LiveView.JS
+  alias Kanta.LocaleInfo
   use KantaWeb, :live_view
 
-  import Kanta.Utils.ParamParsers
-
-  alias Kanta.Translations
-  alias Kanta.Translations.SingularTranslations.Finders.ListSingularTranslations
-  alias Kanta.Translations.PluralTranslations.Finders.ListPluralTranslations
-  alias KantaWeb.Translations.Components.{FiltersBar, MessagesTable}
+  # alias KantaWeb.Translations.Components.FiltersBar
 
   alias KantaWeb.Components.Shared.Pagination
 
-  @available_filters ~w(application_source_id domain_id context_id search not_translated page)
-  @available_params ~w(page search filter)
-  @params_in_filter ~w(application_source_id domain_id context_id not_translated)
-  @ids_to_parse ~w(application_source_id domain_id context_id locale_id)
+  import KantaWeb.Components.Shared.Table
 
-  def mount(%{"locale_id" => locale_id} = params, _session, socket) do
+  # @available_filters ~w(application_source_id domain_id context_id search not_translated page)
+  # @available_params ~w(page search filter)
+  # @params_in_filter ~w(application_source_id domain_id context_id not_translated)
+  # @ids_to_parse ~w(application_source_id domain_id context_id locale_id)
+
+  def mount(_params, session, socket) do
+    data_access = session["data_access"]
+
     socket =
-      case get_locale(locale_id) do
-        {:ok, locale} ->
-          socket
-          |> assign(:locale, locale)
-          |> assign(:application_sources_empty?, Translations.application_sources_empty?())
-          |> assign(get_assigns_from_params(params))
-
-        _ ->
-          socket
-          |> redirect(to: "/kanta/locales")
-      end
+      socket
+      |> assign_data_access(data_access)
 
     {:ok, socket}
   end
 
-  def handle_params(%{"locale_id" => locale_id} = params, _location, socket) do
-    preload_filters = %{"locale_id" => locale_id}
-    singular_translation_query = ListSingularTranslations.filter_query(preload_filters)
-    plural_translation_query = ListPluralTranslations.filter_query(preload_filters)
+  def handle_params(%{"locale_id" => locale_id} = _params, _location, socket) do
+    locale_info = LocaleInfo.get_locale_info(locale_id)
+    data_access = get_data_access(socket)
 
-    %{entries: messages, metadata: messages_metadata} =
-      Translations.list_messages(
-        []
-        |> Keyword.merge(search: params["search"] || "")
-        |> Keyword.merge(page: parse_page(params["page"] || "1"))
-        |> Keyword.merge(
-          filter: parse_filters(Map.put(params["filter"] || %{}, "locale_id", locale_id))
-        )
-        |> Keyword.merge(
-          preloads: [
-            :application_source,
-            :context,
-            :domain,
-            singular_translations: singular_translation_query,
-            plural_translations: plural_translation_query
-          ]
-        )
-      )
+    {:ok, {translations, meta}} =
+      data_access.list_translations(%{filters: %{locale: locale_id}}, [])
+
+    %{page: page_number, total_pages: total_pages} = meta
+    pagination_meta = %{page_number: page_number, total_pages: total_pages}
+
+    {:ok, {domains, _meta}} = data_access.list_resources(:domain, %{}, [])
+    {:ok, {contexts, _meta}} = data_access.list_resources(:context, %{}, [])
+
+    domain_colors =
+      Map.new(domains, fn domain_metadata -> {domain_metadata.name, domain_metadata.color} end)
+
+    context_colors =
+      Map.new(contexts, fn context_metadata -> {context_metadata.name, context_metadata.color} end)
 
     socket =
       socket
-      |> assign(:messages, messages)
-      |> assign(:messages_metadata, messages_metadata)
+      |> assign(
+        locale_info: locale_info,
+        translations: translations,
+        domain_colors: domain_colors,
+        context_colors: context_colors,
+        pagination_meta: pagination_meta
+      )
 
     {:noreply, socket}
   end
 
-  def handle_event("change", filters, socket) do
-    filters = Map.put(filters, "page", "1")
-    query = UriQuery.params(format_filters(Map.merge(socket.assigns.filters, filters)))
+  def handle_event("change", _filters, socket) do
+    # filters = Map.put(filters, "page", "1")
+    # query = UriQuery.params(format_filters(Map.merge(socket.assigns.filters, filters)))
 
-    socket = socket |> assign(:filters, Map.merge(socket.assigns.filters, filters))
+    # socket = socket |> assign(:filters, Map.merge(socket.assigns.filters, filters))
 
-    {:noreply,
-     push_patch(socket,
-       to:
-         "#{dashboard_path(socket)}/locales/#{socket.assigns.locale.id}/translations?" <>
-           URI.encode_query(query)
-     )}
+    # {:noreply,
+    #  push_patch(socket,
+    #    to:
+    #      "#{dashboard_path(socket)}/locales/#{socket.assigns.locale.id}/translations?" <>
+    #        URI.encode_query(query)
+    #  )}
+    {:noreply, socket}
   end
 
   def handle_event("navigate", %{"to" => to}, socket) do
     {:noreply, push_navigate(socket, to: dashboard_path(socket) <> to)}
   end
 
-  def handle_event("page_changed", %{"index" => page_number}, socket) do
-    socket =
-      socket
-      |> assign(
-        :filters,
-        Map.merge(socket.assigns.filters, %{"page" => parse_page(page_number)})
-      )
-
-    query =
-      UriQuery.params(
-        format_filters(Map.merge(socket.assigns.filters, %{"page" => parse_page(page_number)}))
-      )
-
-    {:noreply,
-     push_patch(socket,
-       to:
-         "#{dashboard_path(socket)}/locales/#{socket.assigns.locale.id}/translations?" <>
-           URI.encode_query(query)
-     )}
+  def handle_event("page_changed", %{"index" => _page_number}, socket) do
+    {:noreply, socket}
   end
 
-  defp get_locale(id) do
-    case parse_id_filter(id) do
-      {:ok, id} -> Translations.get_locale(filter: [id: id])
-      _ -> {:error, :id, :invalid}
+  defp assign_data_access(socket, data_access), do: assign(socket, data_access: data_access)
+  defp get_data_access(socket), do: socket.assigns.data_access
+
+  defp print_msgid(msgid) when is_binary(msgid) do
+    case String.length(msgid) > 30 do
+      true -> String.slice(msgid, 0..25) <> "[...]"
+      _ -> msgid
     end
   end
 
-  defp format_filters(filters) do
-    filters
-    |> Map.take(@available_filters)
-    |> Enum.reject(fn {_, value} -> is_nil(value) or value == "" end)
-    |> Enum.reduce([filter: %{}, search: "", page: "1"], &update_filters_acc/2)
+  defp print_msgid(msgid), do: msgid
+
+  defp print_translation(""), do: "Missing"
+  defp print_translation(nil), do: "Missing"
+  defp print_translation(msgstr), do: msgstr
+
+  defp print_domain(nil), do: "None"
+  defp print_domain(domain_name), do: domain_name
+
+  defp print_context(nil), do: "Default"
+  defp print_context(""), do: "Default"
+  defp print_context(context_name), do: context_name
+
+  defp lookup_domain_color(nil, _domain_colors),
+    do: "transparent"
+
+  defp lookup_domain_color(domain_name, domain_colors) do
+    Map.get(domain_colors, domain_name, Kanta.Utils.Colors.default_color()) ||
+      Kanta.Utils.Colors.default_color()
   end
 
-  defp update_filters_acc({"search", value}, acc), do: Keyword.put(acc, :search, value)
-  defp update_filters_acc({"page", value}, acc), do: Keyword.put(acc, :page, value)
+  defp lookup_context_color(nil, _context_colors),
+    do: "transparent"
 
-  defp update_filters_acc({"not_translated", value}, acc) do
-    Keyword.put(acc, :filter, Map.put(acc[:filter] || %{}, "not_translated", value))
+  defp lookup_context_color(context_name, context_colors) do
+    Map.get(context_colors, context_name, Kanta.Utils.Colors.default_color()) ||
+      Kanta.Utils.Colors.default_color()
   end
 
-  defp update_filters_acc({key, value}, acc) do
-    case parse_id_filter(value) do
-      {:ok, id} -> Keyword.put(acc, :filter, Map.put(acc[:filter] || %{}, key, id))
-      _ -> acc
-    end
-  end
-
-  defp get_assigns_from_params(params) do
-    params
-    |> Map.take(@available_params)
-    |> Enum.reduce(%{}, fn {key, value}, acc ->
-      case key do
-        "filter" ->
-          values = Map.take(value, @params_in_filter)
-          Map.merge(acc, values)
-
-        filter_key ->
-          Map.put(acc, filter_key, value)
-      end
-    end)
-    |> then(fn filters ->
-      %{
-        not_translated_default: get_not_translated_default_value(params),
-        filters: filters
-      }
-    end)
-  end
-
-  defp get_not_translated_default_value(%{"filter" => filter}) do
-    case filter["not_translated"] do
-      "true" -> true
-      _ -> false
-    end
-  end
-
-  defp get_not_translated_default_value(_), do: false
-
-  defp parse_filters(filters) do
-    Enum.reduce(filters, %{}, &parse_filter/2)
-  end
-
-  defp parse_filter({key, value}, acc) when key in @ids_to_parse do
-    case parse_id_filter(value) do
-      {:ok, id} -> Map.put(acc, key, id)
-      _ -> acc
-    end
-  end
-
-  defp parse_filter({key, value}, acc) do
-    Map.put(acc, key, value)
-  end
+  defp print_translation_type(%{type: :plural}), do: "Plural"
+  defp print_translation_type(_message), do: "Singular"
 end
